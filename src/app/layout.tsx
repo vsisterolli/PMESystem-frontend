aef76cbfbd5dfd4af6489f87c94052dd65da6a80
vsisterolli <vsisterollI@gmai.com>
2024-02-18 12:58:07 -0300

    prettify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.min.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PME System",
-  description: "Sistema de gestão da Polícia PME - Habbo Hotel",
+    title: "PME System",
+    description: "Sistema de gestão da Polícia PME - Habbo Hotel"
 };
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-    <body className={inter.className}>
-      <ToastContainer theme={"dark"}/>
-      {children}
-      <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-    </body>
-    </html>
-  );
+    return (
+        <html lang="pt-BR">
+            <body className={inter.className}>
+                <ToastContainer theme={"dark"} />
+                {children}
+                <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+            </body>
+        </html>
+    );
 }
